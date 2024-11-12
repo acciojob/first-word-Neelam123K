985f56d787399c87d0c94fb21b54a86cb242c18c
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2024-11-12 12:02:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
   // your code here
+	 s = s.trim();
+	const spaceIndex = str.indexOf(' ');
+	 return spaceIndex === -1 ? str : str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
