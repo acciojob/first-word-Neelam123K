23f3797e9fbc0d1d4b62ab249176245fae00682e
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2024-11-12 12:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstWord(s) {
   // your code here
 	 s = s.trim();
-	const spaceIndex = str.indexOf(' ');
-	 return spaceIndex === -1 ? str : str.substring(0, spaceIndex);
+	const spaceIndex = s.indexOf(' ');
+	 return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
